9f09253adeb3f7be7d49c8e1ab2477db7b454303
AbyssRat <yunothefurry@gmail.com>
2025-11-28 10:16:07 +0100

    1.0

---
+++ src/components/Cikk.jsx
@@ -0,0 +1,9 @@
+const Cikk = () => {
+    return (
+        <article>
+            <h2>Cikk címe</h2>
+            <p>Ez egy példa cikk tartalma.</p>
+        </article>
+    );}
+
+export default Cikk;

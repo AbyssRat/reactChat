556a8462da028640a82ce6e8c47fa91ec40e0ea4
AbyssRat <yunothefurry@gmail.com>
2025-11-28 10:25:58 +0100

    1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Cikk from './components/Cikk.jsx';
 function App() {
-
+const cikkek = [
+  { id: 1, title: 'Első bejegyzés', content: 'Ez az első bejegyzés tartalma.' },
+  { id: 2, title: 'Második bejegyzés', content: 'Ez a második bejegyzés tartalma.' },
+  { id: 3, title: 'Harmadik bejegyzés', content: 'Ez a harmadik bejegyzés tartalma.' }
+];
 
   return (
     <>
 
-    
+
     <Cikk />
     
 
